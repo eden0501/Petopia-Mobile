160bb927eaaee0e1a1dd2e7e72c1a45859a9f590
eden0501 <eden03.b@gmail.com>
2026-03-15 20:53:16 +0200

    change to orange

--- app/src/main/java/com/example/petopia/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/petopia/ui/auth/AuthFragment.kt
@@ -44,10 +44,17 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
 
         etOwnerSince.setOnClickListener {
             val calendar = Calendar.getInstance()
-            DatePickerDialog(requireContext(), { _, year, month, day ->
-                val date = "$day/${month + 1}/$year"
-                etOwnerSince.setText(date)
-            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+            DatePickerDialog(
+                requireContext(),
+                R.style.DatePickerTheme,
+                { _, year, month, day ->
+                    val date = "$day/${month + 1}/$year"
+                    etOwnerSince.setText(date)
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
 
         authTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
