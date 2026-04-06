6c48116ec90067f33aea8371bdd8e338e835b0ae
eden0501 <eden03.b@gmail.com>
2026-04-06 17:18:53 +0300

    chore(database): bump local database schema version to 4

--- app/src/main/java/com/example/petopia/data/local/AppLocalDbRepository.kt
+++ app/src/main/java/com/example/petopia/data/local/AppLocalDbRepository.kt
@@ -6,7 +6,7 @@ import com.example.petopia.data.model.Post
 import com.example.petopia.data.model.User
 import com.example.petopia.util.Converters
 
-@Database(entities = [User::class, Post::class, Comment::class], version = 3, exportSchema = false)
+@Database(entities = [User::class, Post::class, Comment::class], version = 4, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppLocalDbRepository : RoomDatabase() {
     abstract fun userDao(): UserDao
